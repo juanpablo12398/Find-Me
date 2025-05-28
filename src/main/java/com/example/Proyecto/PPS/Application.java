fea1a52787d4825b3e45e03e55f187a2c66e2e52
juanpablo12398 <jpverdondoni@gmail.com>
2025-05-28 16:52:12 -0300

    Cambios en Application.java antes de merge

--- src/main/java/com/example/Proyecto/PPS/Application.java
+++ src/main/java/com/example/Proyecto/PPS/Application.java
@@ -1,4 +1,4 @@
-package com.example.Proyecto.PPS;
+package com.example.proyecto.PPS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
