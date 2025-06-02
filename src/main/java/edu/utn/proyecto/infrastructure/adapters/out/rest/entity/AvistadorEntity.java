70aa70f389765247036d6fb883f54910d48e260f
juanpablo12398 <jpverdondoni@gmail.com>
2025-06-02 20:12:39 -0300

    Commit 5 Cambie el nombre de AvistadoresEntity a AvistadorEntity

--- src/main/java/edu/utn/proyecto/infrastructure/adapters/out/rest/entity/AvistadorEntity.java
+++ src/main/java/edu/utn/proyecto/infrastructure/adapters/out/rest/entity/AvistadorEntity.java
@@ -1,6 +1,6 @@
 package edu.utn.proyecto.infrastructure.adapters.out.rest.entity;
 
-public class AvistadoresEntity {
+public class AvistadorEntity {
     // Esta clase va a contener los mismos atributos que la clase Avistador del dominio,
     // pero con anotaciones de JPA para mapearla a una tabla en la base de datos.
 
