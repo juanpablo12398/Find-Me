0e6b508c006cfc63c3cceb32c91880eb0fe91d4e
juanpablo12398 <jpverdondoni@gmail.com>
2025-06-09 12:24:28 -0300

    Cambie cosas de la estructura para partir de aca.

--- src/main/java/edu/utn/proyecto/applicacion/mappers/AvistadorMapper.java
+++ src/main/java/edu/utn/proyecto/applicacion/mappers/AvistadorMapper.java
@@ -1,6 +1,5 @@
 package edu.utn.proyecto.applicacion.mappers;
 import edu.utn.proyecto.domain.model.Avistador;
-import edu.utn.proyecto.infrastructure.adapters.out.rest.entity.AvistadorEntity;
 
 public class AvistadorMapper {
     // Se traduce de DTO a Entity
