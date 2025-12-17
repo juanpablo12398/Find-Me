216c0563fd8533bb0c5ed42fdfba223d5c01d23c
juanpablo12398 <jpverdondoni@gmail.com>
2025-12-16 21:07:57 -0300

    cambios deploy

--- src/main/java/edu/utn/proyecto/infrastructure/config/JacksonConfig.java
+++ src/main/java/edu/utn/proyecto/infrastructure/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package edu.utn.proyecto.infrastructure.config;
 import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -10,7 +11,7 @@ public class JacksonConfig {
     @Bean
     public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.modules(new JtsModule());
+        builder.modules(new JtsModule(), new JavaTimeModule());
         return builder;
     }
 }
